bc2d15780d53079e96248825bf8cd069ece5a661
Mario <marioeid41@gmail.com>
2024-01-03 16:05:19 -0500

    Test

--- Main.cs
+++ Main.cs
@@ -33,7 +33,7 @@ namespace TopDownShooter
 
             graphics.PreferredBackBufferWidth = Globals.screenWidth;
             graphics.PreferredBackBufferHeight = Globals.screenHeight;
-
+            hii
             graphics.ApplyChanges();
 
             base.Initialize();
